73b977b5a6c4ad5a3d10e6cf5b50f85e86c9d2df
rtvu <rtvu87@gmail.com>
2020-02-25 16:15:17 +0000

    Partial

--- lib/inductive_graph/algorithms/maximum_flow.ex
+++ lib/inductive_graph/algorithms/maximum_flow.ex
@@ -3,6 +3,12 @@ defmodule InductiveGraph.Algorithms.MaximumFlow do
   Functions to calculate maximum flow using the Edmonds-Karp algorithm.
   """
 
+  alias InductiveGraph, as: Graph
+
+  @doc """
+  Creates a list of reversed edges based on `tagged_edges` with 0 maximum
+  capacity.
+  """
   def list_reversed_tagged_edges(tagged_edges) do
     reverse_edges =
       fn
@@ -10,9 +16,41 @@ defmodule InductiveGraph.Algorithms.MaximumFlow do
           [{to_vertex, from_vertex, 0} | accumulator]
       end
 
-    tagged_edges
-    |> Enum.reduce([], reverse_edges)
+    Enum.reduce(tagged_edges, [], reverse_edges)
   end
 
+  def format_graph(graph) do
+    reversed_tagged_edges =
+      graph
+      |> Graph.list_tagged_edges()
+      |> list_reversed_tagged_edges()
 
+    case Graph.insert_tagged_edges(graph, reversed_tagged_edges) do
+      {:ok, graph} -> {:ok, Graph.map_edges(graph, fn i -> {i, 0, i} end)}
+      _error -> :error
+    end
+  end
+
+  def update_adjacents_flow(adjacents, neighbor, flow) do
+    update =
+      fn
+        {{maximum_capacity, current_flow, residual_capacity}, ^neighbor} -> {{maximum_capacity, current_flow + flow, residual_capacity - flow}, neighbor}
+        no_change -> no_change
+      end
+
+    Enum.map(adjacents, update)
+  end
+
+  def update_flow(graph, path, flow)
+  def update_flow(graph, [], _flow), do: graph
+  def update_flow(graph, [_from_vertex], _flow), do: graph
+  def update_flow(graph, [from_vertex, to_vertex | rest], flow) do
+    {:ok, context, graph} = Graph.decompose(graph, from_vertex)
+    {predecessors, vertex, vertex_value, successors} = context
+    predecessors = update_adjacents_flow(predecessors, to_vertex, -flow)
+    successors = update_adjacents_flow(successors, to_vertex, flow)
+    context = {predecessors, vertex, vertex_value, successors}
+    {:ok, graph} = Graph.merge(graph, context)
+    update_flow(graph, [to_vertex | rest], flow)
+  end
 end
