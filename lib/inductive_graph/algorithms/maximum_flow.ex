7a1a1e4dc67d60078a761bd27875b6fc09c669ea
rtvu <rtvu@users.noreply.github.com>
2020-02-25 23:17:57 +0000

    Partial

--- lib/inductive_graph/algorithms/maximum_flow.ex
+++ lib/inductive_graph/algorithms/maximum_flow.ex
@@ -53,4 +53,48 @@ defmodule InductiveGraph.Algorithms.MaximumFlow do
     {:ok, graph} = Graph.merge(graph, context)
     update_flow(graph, [to_vertex | rest], flow)
   end
+
+  def get_maximum_flow(graph, source, sink) do
+    graph = format_graph(graph)
+    graph = get_maximum_flow_helper(graph, source, sink)
+  end
+
+  defp get_maximum_flow_helper(graph, source, sink) do
+
+  end
+
+
+
+
+  def find_path(graph, source, sink) do
+    path = [{nil, source}]
+    queue = :queue.from_list([path])
+    find_path_helper(graph, sink, queue)
+  end
+
+  defp find_path_helper(graph, sink, queue) do
+    if :queue.is_empty(queue) or Graph.empty?(graph) do
+      :error
+    else
+      {{:value, path}, queue} = :queue.out(queue)
+      [{_next_edge_value, next_vertex | _rest_of_path] = path
+      if next_vertex == sink do
+        {:ok, Enum.reverse(path)}
+      else
+        case Graph.decompose(graph, next_vertex) do
+          {:ok, context, graph} ->
+            {_predecessors, _vertex, _vertex_value, successors} = context
+            new_paths = Enum.map(successors, fn successor -> [successor | path] end)
+            queue = :queue.join(queue, :queue.from_list(new_paths))
+            find_path_helper(graph, sink, queue)
+          :error ->
+            find_path_helper(graph, sink, queue)
+        end
+      end
+    end
+  end
+
+
+
+
 end
